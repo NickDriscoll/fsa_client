58ae6a90215a1148bed752881e0a682fde8a8e24
Nick Driscoll <nick.a.mavros@gmail.com>
2018-08-30 01:02:59 -0400

    Next thing to do is make one texture per button

---
+++ core/src/com/mygdx/game/ButtonInput.java
@@ -0,0 +1,9 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.math.Rectangle;
+
+public class ButtonInput extends Rectangle {
+    public ButtonInput(double x, double y) {
+
+    }
+}
--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -10,12 +11,63 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import java.io.IOException;
 import java.net.Socket;
 
-public class Game extends ApplicationAdapter {
+public class Game extends ApplicationAdapter implements InputProcessor {
 	SpriteBatch batch;
-	Texture img;
 	BitmapFont font;
+	Texture controls;
 	Socket socket;
 	boolean connected;
+	boolean touched;
+	ButtonInput[] buttons;
+
+	char input_bitmask;
+
+	const int NUMBER_OF_BUTTONS = 6;
+
+	@Override
+	public boolean keyDown(int a) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int a) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char c) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		touched = true;
+		return true;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		touched = false;
+		return true;
+	}
 
 	private boolean connect_to_server() {
 		//Try all ip addresses looking for one where port 1337 is open
@@ -38,9 +90,19 @@ public class Game extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 		font = new BitmapFont();
 		font.getData().setScale(5f);
+		controls = new Texture(Gdx.files.internal("touch_buttons_0.png"));
+
+		Gdx.input.setInputProcessor(this);
+
+		input_bitmask = 0;
+		touched = false;
+
+		//Initialize the six buttons
+		buttons = new ButtonInput[NUMBER_OF_BUTTONS];
+		buttons[0] = new ButtonInput(0, Gdx.graphics.getHeight() * (5.0 / 6.0));
+
 		connected = connect_to_server();
 	}
 
@@ -48,17 +110,28 @@ public class Game extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		//Get touchscreen state
+
+		//Update
+
+		//Draw
 		batch.begin();
-		if (connected)
-			font.draw(batch, "Connected to server!",Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		else
-			font.draw(batch, "Unable to find server.",Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+		batch.draw(controls, 0, 0, Gdx.graphics.getWidth(), controls.getHeight() * (Gdx.graphics.getWidth() / controls.getWidth()));
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+	}
+
+	enum Buttons {
+		Left,
+		Right,
+		Up,
+		Down,
+		B,
+		A
 	}
 }
